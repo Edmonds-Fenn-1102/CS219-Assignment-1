790733595ddcc401d66a6cadff9c477b129259b9
Edmonds-Fenn-1102 <autumne@nevada.unr.edu>
2023-02-20 18:58:48 -0800

    current commit

---
+++ driver.cpp
@@ -0,0 +1,26 @@
+//
+//Author: Fenn Edmonds
+//Purpose: To implement the ADD function using c++ code
+//Date: 2/20/2023
+//
+
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+//function declaration for adding hex numbers together
+int addFunction(int, int);
+
+int main()
+{
+
+
+
+    return 0;
+}
+
+int addFunction(int firstNum, int secondNum)
+{
+    
+}
